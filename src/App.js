e78fd9e04a37966a1db14f9d30594d94240b4b04
notebook <krawmaciej@gmail.com>
2020-11-17 16:24:23 +0100

    Add landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/play">
+          <Play />
+        </Route>
+      </div>
+      <div>
+        <nav>
+         <Link to="/play">Play!</Link>
+        </nav>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Play() {
+  return <h2>Chessboard.</h2>;
+}
