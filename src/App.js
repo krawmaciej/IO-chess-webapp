82b3d201d99ccdc385863808da23f6cbd486d1fa
notebook <krawmaciej@gmail.com>
2020-11-19 03:01:17 +0100

    Add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+// react
+import React, { Component } from "react";
 import {
+  Switch,
   BrowserRouter as Router,
   Route,
-  Link
+  Redirect
 } from "react-router-dom";
+
+// components
+import Home from './components/home'
+import Login from './components/login'
+import CreateAccount from './components/createAccount'
+import Play from './components/play'
+// firebase
 import { auth } from './firebase/firebase'
+// styles
 import "./App.css";
 
 export default function App() {
