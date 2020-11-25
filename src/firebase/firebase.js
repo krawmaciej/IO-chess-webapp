705f613bd4d1985acb402716df7774194737bb72
dekarboro <53567828+dekarboro@users.noreply.github.com>
2020-11-26 00:01:32 +0200

    credentials

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,21 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FBASE_API_KEY,
+  apiKey: "AIzaSyCREYaGIKnRdtJWqF4flyWOnmkTyL_ftz0", // process.env.REACT_APP_FBASE_API_KEY,
   authDomain: "io-chess-webapp.firebaseapp.com",
   databaseURL: "https://io-chess-webapp.firebaseio.com",
   projectId: "io-chess-webapp",
   storageBucket: "io-chess-webapp.appspot.com",
-  messagingSenderId: process.env.REACT_APP_FBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FBASE_APP_ID
+  messagingSenderId: "263037008208", // process.env.REACT_APP_FBASE_MESSAGING_SENDER_ID,
+  appId: "1:263037008208:web:50b135d6e33e52d3ea9455", // process.env.REACT_APP_FBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth;
 export const db = firebase.database();
+
+/*
+REACT_APP_FBASE_API_KEY=AIzaSyCREYaGIKnRdtJWqF4flyWOnmkTyL_ftz0
+REACT_APP_FBASE_MESSAGING_SENDER_ID=263037008208
+REACT_APP_FBASE_APP_ID=1:263037008208:web:50b135d6e33e52d3ea9455
+*/
