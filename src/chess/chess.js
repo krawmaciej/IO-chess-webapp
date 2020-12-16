b9738150496d285b548e3ed0d3d381b544bcb77d
JaroslawCzerwik <40338319+JaroslawCzerwik@users.noreply.github.com>
2020-12-16 19:14:18 +0100

    CHESS_COLORS refactoring

--- src/chess/chess.js
+++ src/chess/chess.js
@@ -40,11 +40,11 @@ class Pawn extends ChessPiece {
     super(color, posX, posY);
     
     this.type = 'Pawn';
-    this.code = this.color === 'w' ? "&#9817;" : "&#9823;";
+    this.code = this.color === 0 ? "&#9817;" : "&#9823;";
   }
 
   calculateMoves() {
-    if (this.color !== 'w')
+    if (this.color !== 0)
       return;
     // clear old data (bad place to do it, I know)
     this.possibleMoves.splice(0);
@@ -65,11 +65,11 @@ class Rook extends ChessPiece {
     super(color, posX, posY);
 
     this.type = 'Rook';
-    this.code = this.color === 'w' ? '&#9814' : '&#9820';
+    this.code = this.color === 0 ? '&#9814' : '&#9820';
   }
 
   calculateMoves() {
-    if (this.color !== 'w')
+    if (this.color !== 0)
       return;
     // clear old data (bad place to do it, I know)
     this.possibleMoves.splice(0);
@@ -89,11 +89,11 @@ class Knight extends ChessPiece {
     super(color, posX, posY);
 
     this.type = 'Knight';
-    this.code = this.color === 'w' ? '&#9816' : '&#9822';
+    this.code = this.color === 0 ? '&#9816' : '&#9822';
   }
 
   calculateMoves() {
-    if (this.color !== 'w')
+    if (this.color !== 0)
       return;
     // clear old data (bad place to do it, I know)
     this.possibleMoves.splice(0);
@@ -122,11 +122,11 @@ class Bishop extends ChessPiece {
     super(color, posX, posY);
 
     this.type = 'Bishop';
-    this.code = this.color === 'w' ? '&#9815' : '&#9821';
+    this.code = this.color === 0 ? '&#9815' : '&#9821';
   }
 
   calculateMoves() {
-    if (this.color !== 'w')
+    if (this.color !== 0)
       return;
     // clear old data (bad place to do it, I know)
     this.possibleMoves.splice(0);
@@ -166,11 +166,11 @@ class King extends ChessPiece {
     super(color, posX, posY);
 
     this.type = 'King';
-    this.code = this.color === 'w' ? '&#9812' : '&#9818'; 
+    this.code = this.color === 0 ? '&#9812' : '&#9818'; 
   }
 
   calculateMoves() {
-    if (this.color !== 'w')
+    if (this.color !== 0)
       return;
     // clear old data (bad place to do it, I know)
     this.possibleMoves.splice(0);
@@ -194,11 +194,11 @@ class Queen extends ChessPiece {
     super(color, posX, posY);
 
     this.type = 'Queen';
-    this.code = this.color === 'w' ? '&#9813' : '&#9819';
+    this.code = this.color === 0 ? '&#9813' : '&#9819';
   }
 
   calculateMoves() {
-    if (this.color !== 'w')
+    if (this.color !== 0)
       return;
     // clear old data (bad place to do it, I know)
     this.possibleMoves.splice(0);
@@ -237,39 +237,39 @@ class Queen extends ChessPiece {
 // initial board state for every chest match
 export const initChessPieces = () => {
   return {
-    wPawn0: new Pawn('w', 6, 0),
-    wPawn1: new Pawn('w', 6, 1),
-    wPawn2: new Pawn('w', 6, 2),
-    wPawn3: new Pawn('w', 6, 3),
-    wPawn4: new Pawn('w', 6, 4),
-    wPawn5: new Pawn('w', 6, 5),
-    wPawn6: new Pawn('w', 6, 6),
-    wPawn7: new Pawn('w', 6, 7),
-    wRook0: new Rook('w', 7, 0),
-    wRook1: new Rook('w', 7, 7),
-    wKnight0: new Knight('w', 7, 1),
-    wKnight1: new Knight('w', 7, 6),
-    wBishop0: new Bishop('w', 7, 2),
-    wBishop1: new Bishop('w', 7, 5),
-    wKing: new King('w', 7, 4),
-    wQueen: new Queen('w', 7, 3),
+    wPawn0: new Pawn(0, 6, 0),
+    wPawn1: new Pawn(0, 6, 1),
+    wPawn2: new Pawn(0, 6, 2),
+    wPawn3: new Pawn(0, 6, 3),
+    wPawn4: new Pawn(0, 6, 4),
+    wPawn5: new Pawn(0, 6, 5),
+    wPawn6: new Pawn(0, 6, 6),
+    wPawn7: new Pawn(0, 6, 7),
+    wRook0: new Rook(0, 7, 0),
+    wRook1: new Rook(0, 7, 7),
+    wKnight0: new Knight(0, 7, 1),
+    wKnight1: new Knight(0, 7, 6),
+    wBishop0: new Bishop(0, 7, 2),
+    wBishop1: new Bishop(0, 7, 5),
+    wKing: new King(0, 7, 4),
+    wQueen: new Queen(0, 7, 3),
 
-    bPawn0: new Pawn('b', 1, 0),
-    bPawn1: new Pawn('b', 1, 1),
-    bPawn2: new Pawn('b', 1, 2),
-    bPawn3: new Pawn('b', 1, 3),
-    bPawn4: new Pawn('b', 1, 4),
-    bPawn5: new Pawn('b', 1, 5),
-    bPawn6: new Pawn('b', 1, 6),
-    bPawn7: new Pawn('b', 1, 7),
-    bRook0: new Rook('b', 0, 0),
-    bRook1: new Rook('b', 0, 7),
-    bKnight0: new Knight('b', 0, 1),
-    bKnight1: new Knight('b', 0, 6),
-    bBishop0: new Bishop('b', 0, 2),
-    bBishop1: new Bishop('b', 0, 5),
-    bKing: new King('b', 0, 4),
-    bQueen: new Queen('b', 0, 3)
+    bPawn0: new Pawn(0, 1, 0),
+    bPawn1: new Pawn(0, 1, 1),
+    bPawn2: new Pawn(0, 1, 2),
+    bPawn3: new Pawn(0, 1, 3),
+    bPawn4: new Pawn(0, 1, 4),
+    bPawn5: new Pawn(0, 1, 5),
+    bPawn6: new Pawn(0, 1, 6),
+    bPawn7: new Pawn(0, 1, 7),
+    bRook0: new Rook(0, 0, 0),
+    bRook1: new Rook(0, 0, 7),
+    bKnight0: new Knight(0, 0, 1),
+    bKnight1: new Knight(0, 0, 6),
+    bBishop0: new Bishop(0, 0, 2),
+    bBishop1: new Bishop(0, 0, 5),
+    bKing: new King(0, 0, 4),
+    bQueen: new Queen(0, 0, 3)
   }
 }
 
--- src/chess/chessboard.js
+++ src/chess/chessboard.js
@@ -15,15 +15,15 @@ export const initBoard = (cp) => {
   board[cp.wPawn5.posX][cp.wPawn5.posY] = cp.wPawn5;
   board[cp.wPawn6.posX][cp.wPawn6.posY] = cp.wPawn6;
   board[cp.wPawn7.posX][cp.wPawn7.posY] = cp.wPawn7;
-  board[cp.wRook0.posX][cp.wRook0.posY] = cp.wRook0;    
+  board[cp.wRook0.posX][cp.wRook0.posY] = cp.wRook0;
   board[cp.wRook1.posX][cp.wRook1.posY] = cp.wRook1;
   board[cp.wKnight0.posX][cp.wKnight0.posY] = cp.wKnight0;
   board[cp.wKnight1.posX][cp.wKnight1.posY] = cp.wKnight1;
-  board[cp.wBishop0.posX][cp.wBishop0.posY] = cp.wBishop0;  
-  board[cp.wBishop1.posX][cp.wBishop1.posY] = cp.wBishop1; 
-  board[cp.wKing.posX][cp.wKing.posY] = cp.wKing;  
-  board[cp.wQueen.posX][cp.wQueen.posY] = cp.wQueen;   
-  
+  board[cp.wBishop0.posX][cp.wBishop0.posY] = cp.wBishop0;
+  board[cp.wBishop1.posX][cp.wBishop1.posY] = cp.wBishop1;
+  board[cp.wKing.posX][cp.wKing.posY] = cp.wKing;
+  board[cp.wQueen.posX][cp.wQueen.posY] = cp.wQueen;
+
   // BLACK
   board[cp.bPawn0.posX][cp.bPawn0.posY] = cp.bPawn0;
   board[cp.bPawn1.posX][cp.bPawn1.posY] = cp.bPawn1;
@@ -33,17 +33,17 @@ export const initBoard = (cp) => {
   board[cp.bPawn5.posX][cp.bPawn5.posY] = cp.bPawn5;
   board[cp.bPawn6.posX][cp.bPawn6.posY] = cp.bPawn6;
   board[cp.bPawn7.posX][cp.bPawn7.posY] = cp.bPawn7;
-  board[cp.bRook0.posX][cp.bRook0.posY] = cp.bRook0;    
+  board[cp.bRook0.posX][cp.bRook0.posY] = cp.bRook0;
   board[cp.bRook1.posX][cp.bRook1.posY] = cp.bRook1;
   board[cp.bKnight0.posX][cp.bKnight0.posY] = cp.bKnight0;
   board[cp.bKnight1.posX][cp.bKnight1.posY] = cp.bKnight1;
-  board[cp.bBishop0.posX][cp.bBishop0.posY] = cp.bBishop0;  
-  board[cp.bBishop1.posX][cp.bBishop1.posY] = cp.bBishop1; 
-  board[cp.bKing.posX][cp.bKing.posY] = cp.bKing;  
-  board[cp.bQueen.posX][cp.bQueen.posY] = cp.bQueen; 
-  
+  board[cp.bBishop0.posX][cp.bBishop0.posY] = cp.bBishop0;
+  board[cp.bBishop1.posX][cp.bBishop1.posY] = cp.bBishop1;
+  board[cp.bKing.posX][cp.bKing.posY] = cp.bKing;
+  board[cp.bQueen.posX][cp.bQueen.posY] = cp.bQueen;
+
   // BLANK
-  for (let i = 0; i < 8; i++) 
+  for (let i = 0; i < 8; i++)
     for (let j = 2; j < 6; j++)
       board[j][i] = {};
   return board;
@@ -59,7 +59,7 @@ export const drawBoard = (moveListener) => { // pass a listener to be called whe
   // draw tiles
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-      container.appendChild(makeTile(toggle, i, j));      
+      container.appendChild(makeTile(toggle, i, j));
       toggle = !toggle;
     }
     toggle = !toggle;
@@ -67,18 +67,18 @@ export const drawBoard = (moveListener) => { // pass a listener to be called whe
 
   // add onclick event listeners
   const tiles = document.getElementById('chessBoard').children;
-  
+
   for (let i = 0; i < tiles.length; i++) {
     tiles[i].addEventListener('click', () => {
       moveChessPiece(tiles[i], moveListener);
-	  //console.log('tiles children: ', tiles[i]._cp);
+      //console.log('tiles children: ', tiles[i]._cp);
     });
-  }  
+  }
 }
 // create chess board tile element
 export const makeTile = (color, x, y) => {
   const elem = document.createElement('div');
-  elem.classList.add('boardSquare', color ? 'boardSquareWhite' : 'boardSquareBlack' );
+  elem.classList.add('boardSquare', color ? 'boardSquareWhite' : 'boardSquareBlack');
   elem.id = x + '_' + y;
   elem.innerHTML = EMPTY_TILE; // draw tiles containing empty char in div so that board can be resizable
 
@@ -108,8 +108,8 @@ export const insertChessPiece = (piece) => {
 // ex: {from: "6_6", to: "5_6"}, and then moving correct piece from to
 // however, for now it works so it can be left like that
 export const movePieceWithoutChecking = (moveFromServer) => {
-  const move = prepareMove(moveFromServer); 
-	// move visual chess piece
+  const move = prepareMove(moveFromServer);
+  // move visual chess piece
   move.to.innerHTML = move.from.innerHTML;
   move.from.innerHTML = EMPTY_TILE;
   // change colors of visual pieces (display pieces only)
@@ -120,7 +120,7 @@ export const movePieceWithoutChecking = (moveFromServer) => {
   move.from._cp = false;
   // update current chess piece object
   //move.to._cp.active = false; // ?? this could be deleted but not sure
-  move.to._cp.setPositions(parseInt(move.to.id.substr(0,1)), parseInt(move.to.id.substr(2,1)));
+  move.to._cp.setPositions(parseInt(move.to.id.substr(0, 1)), parseInt(move.to.id.substr(2, 1)));
 }
 
 const prepareMove = (move) => {
@@ -136,7 +136,7 @@ export const moveChessPiece = (elem, moveListener) => {
   const activeElem = parent.activeElement;
 
   // if chess piece is no chosen, choose it and save relevant data
-  if (elem._cp && !elem.parentElement.switcher) {    
+  if (elem._cp && !elem.parentElement.switcher) {
     //elem._cp.active = true; // ?? this could be deleted but not sure
     elem.parentElement.switcher = true;
     elem.parentElement.activeElement = elem;
@@ -145,12 +145,12 @@ export const moveChessPiece = (elem, moveListener) => {
     // calculate possible moves for chess piece
     elem._cp.calculateMoves();
     console.log('possible moves --> ', elem._cp.possibleMoves); // TEMP
-  } 
+  }
   // check if chess piece is chosen, if yes, try to move it
-  else if (parent.switcher && activeElem) {    
+  else if (parent.switcher && activeElem) {
     // check is new tile and empty, if yes, move chess piece there
     if (!elem._cp && elem.innerHTML === EMPTY_TILE && activeElem._cp.moveIsPossible(elem.id)) {
-      moveListener({from: activeElem.id, to: elem.id}); // signal the listener about move
+      moveListener({ from: activeElem.id, to: elem.id }); // signal the listener about move
       //movePieceWithoutChecking(move); // don't make a move, instead let server update the board with this move
       //console.log('move in chessboard move normal f(): ', move);
     }
@@ -158,7 +158,7 @@ export const moveChessPiece = (elem, moveListener) => {
     activeElem.classList.toggle('active'); // no active color on tile
     //activeElem._cp.active = false; // ?? this could be deleted but not sure
     parent.switcher = false;
-  }  
-}
+  }
+};
 
 
