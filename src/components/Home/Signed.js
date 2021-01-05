a99b5c3afd0d9907ef067b675b13b94df9f44907
notebook <krawmaciej@gmail.com>
2021-01-05 15:15:34 +0100

    Working play button

--- src/components/Home/Signed.js
+++ src/components/Home/Signed.js
@@ -1,68 +1,67 @@
-import React, { Component } from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
+
 import { CHESS_COLORS } from '../../chess/chess';
 import { auth, database } from '../../firebase/firebase';
 import { userCachedData } from '../../user/userData';
 
-export default class Signed extends Component {
-    render() {
-        return (
-            <div>
-                <p>Logged in as {userCachedData.email}</p>
-                <p>uid is {userCachedData.uid}</p> {/* TODO: uid for tests, remove later*/}
-                <p>gid is {userCachedData.gameId}</p> {/* TODO: gid for tests, remove later*/}
-                <button onClick={this.playGame}>Play</button>
-                <button onClick={this.SignOut}>Wyloguj</button>
-            </div>
-        );
-    }
+export default function Signed() {
+    const history = useHistory();
 
-    SignOut() {
+    function SignOut() {
         auth().signOut().then(() => {
           console.log('successful signout');
           // Sign-out successful.
         }).catch(console.log)
     }
 
-    // TODO: move to /play
-    // if player not in a game create a quick game and wait for other player to join
-    // if in a game then yeah...
-    playGame() {
-      if (userCachedData.gameId === "") { // create new game
-        this.createGame(this.props.history);
-      } else { // player is already in a game
-        this.goToGame(this.props.history);
+    function playGame() {
+        if (userCachedData.gameId === "") { // create new game
+          //console.log("history: ", this.props.history);
+          // console.log("props: ", this.props);
+          createGame();
+        } else { // player is already in a game
+          goToGame()
+        }
       }
-    }
-
-    createGame(history) {
-        const newGame = { // game state to be pushed into database
-            whitePlayerUid: "",
-            blackPlayerUid: "",
-            activePlayerColor: CHESS_COLORS.WHITE,
-            winner: "",
-            moves: [] // array of moves made by each player, also used to load current state of the board
-        };
-
-        newGame.whitePlayerUid = userCachedData.uid; // make player a white player
-        userCachedData.color = CHESS_COLORS.WHITE; // set player color to white
-
-        const gameRef = database().ref("games").push();
-        userCachedData.gameId = gameRef.key; // set user gid
-
-        const loggedUserRef = database().ref("users").child(userCachedData.uid);
-        loggedUserRef.set(userCachedData); // update user data on database
-
-        gameRef.set(newGame).then(() => {
-            history.push("/play");
-        }, (err) => {
-            throw err;
-        });
-    }
       
-    goToGame(history) {
-        // TODO: check first if game exists in the database, if not throw error
-        history.push("/play");
-    }
+      function createGame() {
+          const newGame = { // game state to be pushed into database
+              whitePlayerUid: "",
+              blackPlayerUid: "",
+              activePlayerColor: CHESS_COLORS.WHITE,
+              winner: "",
+              moves: [] // array of moves made by each player, also used to load current state of the board
+          };
       
+          newGame.whitePlayerUid = userCachedData.uid; // make player a white player
+          userCachedData.color = CHESS_COLORS.WHITE; // set player color to white
+      
+          const gameRef = database().ref("games").push();
+          userCachedData.gameId = gameRef.key; // set user gid
+      
+          const loggedUserRef = database().ref("users").child(userCachedData.uid);
+          loggedUserRef.set(userCachedData); // update user data on database
+      
+          gameRef.set(newGame).then(() => {
+              history.push("/play");
+          }, (err) => {
+              throw err;
+          });
+      }
+        
+      function goToGame() {
+          // TODO: check first if game exists in the database, if not throw error
+          history.push("/play");
+      }
 
+    return (
+        <div>
+            <p>Logged in as {userCachedData.email}</p>
+            <p>uid is {userCachedData.uid}</p> {/* TODO: uid for tests, remove later*/}
+            <p>gid is {userCachedData.gameId}</p> {/* TODO: gid for tests, remove later*/}
+            <button onClick={playGame}>Play</button>
+            <button onClick={SignOut}>Wyloguj</button>
+        </div>
+    );
 }
