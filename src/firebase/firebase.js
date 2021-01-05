b71c82dccd602cabc07215b87c511ae009c246cb
notebook <krawmaciej@gmail.com>
2021-01-05 17:37:05 +0100

    Use database settings from .env.local
    Because there are two databases connected

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,13 +16,13 @@ qwerty
 */
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbiN8dJGYZf3WKMSKS-eh7THGNiVOdjyE",
-  authDomain: "io-chess-online.firebaseapp.com",
-  databaseURL: 'https://io-chess-online-default-rtdb.firebaseio.com/',
-  projectId: "io-chess-online",
-  storageBucket: "io-chess-online.appspot.com",
-  messagingSenderId: "596116793454",
-  appId: "1:596116793454:web:4ba1c03df6344a58321e7f"
+  apiKey: process.env.REACT_APP_FBASE_API_KEY,
+  authDomain: "io-chess-webapp.firebaseapp.com",
+  databaseURL: "https://io-chess-webapp.firebaseio.com",
+  projectId: "io-chess-webapp",
+  storageBucket: "io-chess-webapp.appspot.com",
+  messagingSenderId: process.env.REACT_APP_FBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -41,6 +41,17 @@ const firebaseConfig = {
   storageBucket: "io-chess-webapp.appspot.com",
   messagingSenderId: "263037008208", //process.env.REACT_APP_FBASE_MESSAGING_SENDER_ID,
   appId: "1:263037008208:web:50b135d6e33e52d3ea9455", //process.env.REACT_APP_FBASE_APP_ID
+};
+
+dekar config
+const firebaseConfig = {
+  apiKey: "AIzaSyAbiN8dJGYZf3WKMSKS-eh7THGNiVOdjyE",
+  authDomain: "io-chess-online.firebaseapp.com",
+  databaseURL: 'https://io-chess-online-default-rtdb.firebaseio.com/',
+  projectId: "io-chess-online",
+  storageBucket: "io-chess-online.appspot.com",
+  messagingSenderId: "596116793454",
+  appId: "1:596116793454:web:4ba1c03df6344a58321e7f"
 };
  */
 
