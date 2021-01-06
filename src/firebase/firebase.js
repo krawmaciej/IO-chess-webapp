c41f69c3c08e5d97a2eee312142f9058bb3b5fc1
Maciej Krawczyk <58012109+krawmaciej@users.noreply.github.com>
2021-01-06 17:13:49 +0100

    Restore firebase connection config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,7 +5,6 @@ import firebase from 'firebase';
 -----Account info-----
 login:  IO.chessonline.CGK
 passwd:  4kuGNbsqBM
-
 -----Players-----
 player1@emial.com
 qwerty
@@ -16,13 +15,13 @@ qwerty
 */
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FBASE_API_KEY,
-  authDomain: "io-chess-webapp.firebaseapp.com",
-  databaseURL: "https://io-chess-webapp.firebaseio.com",
-  projectId: "io-chess-webapp",
-  storageBucket: "io-chess-webapp.appspot.com",
-  messagingSenderId: process.env.REACT_APP_FBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FBASE_APP_ID
+  apiKey: "AIzaSyAbiN8dJGYZf3WKMSKS-eh7THGNiVOdjyE",
+  authDomain: "io-chess-online.firebaseapp.com",
+  databaseURL: 'https://io-chess-online-default-rtdb.firebaseio.com/',
+  projectId: "io-chess-online",
+  storageBucket: "io-chess-online.appspot.com",
+  messagingSenderId: "596116793454",
+  appId: "1:596116793454:web:4ba1c03df6344a58321e7f"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -42,16 +41,4 @@ const firebaseConfig = {
   messagingSenderId: "263037008208", //process.env.REACT_APP_FBASE_MESSAGING_SENDER_ID,
   appId: "1:263037008208:web:50b135d6e33e52d3ea9455", //process.env.REACT_APP_FBASE_APP_ID
 };
-
-dekar config
-const firebaseConfig = {
-  apiKey: "AIzaSyAbiN8dJGYZf3WKMSKS-eh7THGNiVOdjyE",
-  authDomain: "io-chess-online.firebaseapp.com",
-  databaseURL: 'https://io-chess-online-default-rtdb.firebaseio.com/',
-  projectId: "io-chess-online",
-  storageBucket: "io-chess-online.appspot.com",
-  messagingSenderId: "596116793454",
-  appId: "1:596116793454:web:4ba1c03df6344a58321e7f"
-};
  */
-
